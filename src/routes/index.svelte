f799e242f86b20a5bc3136af482309127635b3ac
ulwlu <ooulwluoo@gmail.com>
2021-01-12 03:42:06 +0900

    Delete Reddit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,8 +8,8 @@
   <h2>Profile</h2>
   <p>I am ulwlu, an engineer in Tokyo.</p>
   <h2>SNS</h2>
-  <p><a href="https://github.com/ulwlu" target="_blank">Github</a>(<a href="https://github.com/pulls?q=is%3Apr+author%3Aulwlu+archived%3Afalse+sort%3Aupdated-desc+-user%3Aulwlu" target="_blank">PRs</a>) / <a href="https://gist.github.com/ulwlu" target="_blank">Gist</a> / <a href="https://www.linkedin.com/in/kohei-murakami-ulwlu" target="_blank">LinkedIn</a></p>
-  <p><a href="https://qiita.com/ulwlu" target="_blank">Qiita</a> / <a href="https://zenn.dev/ulwlu" target="_blank">Zenn</a> / <a href="https://www.instagram.com/ooulwluoo" target="_blank">Instagram</a> / <a href="https://www.reddit.com/user/ryuta69" target="_blank">Reddit</a></p>
+  <p><a href="https://github.com/ulwlu" target="_blank">Github </a>(<a href="https://github.com/pulls?q=is%3Apr+author%3Aulwlu+archived%3Afalse+sort%3Aupdated-desc+-user%3Aulwlu" target="_blank">my contributions</a>) / <a href="https://gist.github.com/ulwlu" target="_blank">Gist</a></p>
+  <p><a href="https://qiita.com/ulwlu" target="_blank">Qiita</a> / <a href="https://zenn.dev/ulwlu" target="_blank">Zenn</a> / <a href="https://www.instagram.com/ooulwluoo" target="_blank">Instagram</a> / <a href="https://www.linkedin.com/in/kohei-murakami-ulwlu" target="_blank">LinkedIn</a></p>
   <p>Mail: <a href="mailto:ooulwluoo@gmail.com" target="_blank">ooulwluoo@gmail.com</a></p>
   <h2>Thanks</h2>
   <p>This blog is made by Svelte, Sapper, TypeScript, TailwindCSS, Sass, PostCSS, Rollup + netlify/vercel/gh-pages.</p>
