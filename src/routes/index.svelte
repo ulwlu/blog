8585e998cecca72864401eb771aefae100312b1b
ulwlu <ooulwluoo@gmail.com>
2021-05-12 15:59:59 +0900

    Add a email

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,7 @@
   <h2>SNS</h2>
   <p><a href="https://www.instagram.com/ooulwluoo" target="_blank">Instagram</a></p>
   <p><a href="https://github.com/ulwlu" target="_blank">Github</a> / <a href="https://zenn.dev/ulwlu" target="_blank">Zenn</a> / <a href="https://qiita.com/ulwlu" target="_blank">Qiita</a></p>
+  <p>Mail: <a href="mailto:ooulwluoo@gmail.com" target="_blank">ooulwluoo@gmail.com</a></p>
   <h2>Thanks</h2>
   <p>This blog is made by Svelte, Sapper, TypeScript, TailwindCSS, Sass, PostCSS and Rollup on Netlify. Repository is <a href="https://github.com/ulwlu/blog" target="_blank">here</a>.</p>
 </div>
