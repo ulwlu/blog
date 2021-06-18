7a9ba59d5067d38371b0752282faaf06f1ca73c1
ulwlu <ooulwluoo@gmail.com>
2021-06-18 15:18:58 +0900

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,8 +5,7 @@
 
 <div id="profile-wrapper">
   <h2>Profile</h2>
-  <p>I am ulwlu, an engineer in Tokyo.</p>
-  <p><a href="https://github.com/pulls?q=is%3Apr+author%3Aulwlu+archived%3Afalse+sort%3Aupdated-desc+-user%3Aulwlu">Contributing to Open Source Projects</a> and <a href="https://b.hatena.ne.jp/ulwlu/bookmark">writing articles</a>.</p>
+  <p>I am ulwlu, an engineer in Tokyo. I like <a href="https://github.com/pulls?q=is%3Apr+author%3Aulwlu+archived%3Afalse+sort%3Aupdated-desc+-user%3Aulwlu">contributing to Open Source Projects</a> and <a href="https://b.hatena.ne.jp/ulwlu/bookmark">writing articles</a>.</p>
   <h2>SNS</h2>
   <p><a href="https://www.instagram.com/ooulwluoo" target="_blank">Instagram</a></p>
   <p><a href="https://github.com/ulwlu" target="_blank">Github</a> / <a href="https://zenn.dev/ulwlu" target="_blank">Zenn</a> / <a href="https://qiita.com/ulwlu" target="_blank">Qiita</a></p>
