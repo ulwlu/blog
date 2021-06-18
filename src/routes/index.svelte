323dc5e3910011793fc72617dc9546f6f68671c8
ulwlu <ooulwluoo@gmail.com>
2021-06-18 11:21:44 +0900

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,11 +6,11 @@
 <div id="profile-wrapper">
   <h2>Profile</h2>
   <p>I am ulwlu, an engineer in Tokyo.</p>
-  <p>Hobbist of <a href="https://github.com/pulls?q=is%3Apr+author%3Aulwlu+archived%3Afalse+sort%3Aupdated-desc+-user%3Aulwlu"> contributing to Open Source Projects</a> and <a href="https://b.hatena.ne.jp/ulwlu/bookmark">writing articles</a>.</p>
+  <p><a href="https://github.com/pulls?q=is%3Apr+author%3Aulwlu+archived%3Afalse+sort%3Aupdated-desc+-user%3Aulwlu">Contributing to Open Source Projects</a> and <a href="https://b.hatena.ne.jp/ulwlu/bookmark">writing articles</a>.</p>
   <h2>SNS</h2>
   <p><a href="https://www.instagram.com/ooulwluoo" target="_blank">Instagram</a></p>
   <p><a href="https://github.com/ulwlu" target="_blank">Github</a> / <a href="https://zenn.dev/ulwlu" target="_blank">Zenn</a> / <a href="https://qiita.com/ulwlu" target="_blank">Qiita</a></p>
-  <p>Mail: <a href="mailto:ooulwluoo@gmail.com" target="_blank">ooulwluoo@gmail.com</a> (I'm always open if it's about programming or hiring interest)</p>
+  <p>Mail: <a href="mailto:ooulwluoo@gmail.com" target="_blank">ooulwluoo@gmail.com</a></p>
   <h2>Thanks</h2>
   <p>This blog is made by Svelte, Sapper, TypeScript, TailwindCSS, Sass, PostCSS and Rollup on Netlify. Repository is <a href="https://github.com/ulwlu/blog" target="_blank">here</a>.</p>
 </div>
